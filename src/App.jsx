e9150b247a7122a4ef6233a2e94c3d4cfbf57274
Giedre <giedre.rageliene@gmail.com>
2023-07-24 16:13:08 +0300

    updated logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import style from './App.module.css';
 function App() {
   return (
     <div className={style.app}>
-      <img className={style.logoImg} src='./img/logoImg.png' alt='logo' />
-      <h1 className={style.mainTitle}>Shop with me</h1>
+      <img className={style.logoImg} src='./img/logo.png' alt='logo' />
     </div>
   );
 }
