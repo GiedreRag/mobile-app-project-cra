71e00ea06ff01c134726595d6a113c9a77efcc4d
Giedre <giedre.rageliene@gmail.com>
2023-07-24 16:37:57 +0300

    button styling first stage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className={style.app}>
       <img className={style.logoImg} src='./img/logo.png' alt='logo' />
+      <button className={style.button}>Get started</button>
     </div>
   );
 }
