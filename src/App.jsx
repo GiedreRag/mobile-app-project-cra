c24576c431c1745402825f4f3a3f7ad2efe629c7
Giedre <giedre.rageliene@gmail.com>
2023-07-27 12:44:29 +0300

    First routes and some other general changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import style from './App.module.css';
+import { Home } from './pages/Home';
+import { Feature1 } from './pages/features/Feature1';
+import { Feature2 } from './pages/features/Feature2';
+import { Feature3 } from './pages/features/Feature3';
+import { Page404 } from "./pages/Page404";
 
 function App() {
   return (
     <div className={style.app}>
-      <div className={style.homePage}>
-        <img className={style.homeLogoImg} src='./img/logo.png' alt='logo' />
-        <h1 className={style.homeTitle}>Want to enjoy shopping?</h1>
-        <button className={style.button} href="#">Get started</button>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route >
+            <Route path='/' index element={<Home />} />
+            <Route path='/feature/1' element={<Feature1 />} />
+            <Route path='/feature/2' element={<Feature2 />}/>
+            <Route path='/feature/3' element={<Feature3 />}/>
+            <Route path='*' element={<Page404 />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import style from '.././Header.module.css';
+
+export function About() {
+    return (
+        <div className={style.about}>
+            ABOUT PAGE
+        </div>
+    );
+}
