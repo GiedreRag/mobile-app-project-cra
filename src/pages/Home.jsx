91021b2f6fae37b523605ebaf2f6bfac8e710356
Giedre <giedre.rageliene@gmail.com>
2023-07-27 12:43:59 +0300

    Home page and css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import style from '.././App.module.css';
+import style2 from '../components/Button.module.css';
+import logo from '../img/logo.png';
+import { Link } from 'react-router-dom';
+
+export function Home() {
+    return (
+        <div>
+            <div className={style.homePage}>
+                <img className={style.logoImg} src={logo} alt='logo' />
+                <h1 className={style.title}>Want to enjoy shopping?</h1>
+                <Link className={style2.button} href="#" to="/feature/1">Get started!</Link>
+            </div>
+        </div>
+    );
+}
