97949388cf2844ecf2f60d13aee39f546a06d4fb
Giedre <giedre.rageliene@gmail.com>
2023-07-30 09:59:32 +0300

    Img path change

--- src/components/AccountContent.jsx
+++ src/components/AccountContent.jsx
@@ -1,10 +1,10 @@
 import style from '../pages/Account.module.css';
 
-export function AccountContent({dataContent: {title, img, link}}) {
+export function AccountContent({dataContent: {title, link}, imgPath}) {
     return (
         <div className={style.contents}>
             <h3 className={style.contentsTitle}>{title}</h3>
-            <p><img className={style.contentImg} src={`./img/${img}`} alt="logo" /></p>
+            <p><img className={style.contentImg} src={imgPath} alt="product" /></p>
             <p>Buy it <a href={link} target="_blank" rel="noopener noreferrer">here</a></p>
         </div>
     );
