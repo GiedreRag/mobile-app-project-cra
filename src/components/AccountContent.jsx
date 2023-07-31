ca935232c74511ed3dfd9a2f9cc2f0d7e7733f0c
Giedre <giedre.rageliene@gmail.com>
2023-07-31 14:39:38 +0300

    img

--- src/components/AccountContent.jsx
+++ src/components/AccountContent.jsx
@@ -1,10 +1,11 @@
 import style from '../pages/Account.module.css';
 
 export function AccountContent({dataContent: {title, img, link}}) {
+    console.log({img});
     return (
         <div className={style.contents}>
             <h3 className={style.contentsTitle}>{title}</h3>
-            <p><img className={style.contentImg} src={`./img/${img}`} alt="product" /></p>
+            <p><img className={style.contentImg} src={`https://raw.githubusercontent.com/GiedreRag/mobile-app-project-cra/master/public/img/${img}`} alt="product" /></p>
             <p>Buy it <a href={link} target="_blank" rel="noopener noreferrer">here</a></p>
         </div>
     );
